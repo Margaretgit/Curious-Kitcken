0ab865304d61bae8afef1613ae8858a2d4bacb01
Margaret <52404414+Margaretgit@users.noreply.github.com>
2026-09-11 02:58:24 +0100

    Delete Curious Kitchen directory

--- Curious Kitchen/static/script.js
+++
@@ -1,6 +0,0 @@
-function toggleAnswer(button) {
-    const answer = button.nextElementSibling;
-    answer.classList.toggle("show");
-    const icon = button.querySelector("span");
-    icon.textContent = answer.classList.contains("show") ? "−" : "+";
-}
